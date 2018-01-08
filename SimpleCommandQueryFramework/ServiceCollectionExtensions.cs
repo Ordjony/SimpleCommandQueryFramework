354269b77d9ec41eb1dbdfd627eae11c32555bd9
Ordjony <ordjony@outlook.com>
2018-01-08 15:54:41 +0200

    add small register extensions

--- SimpleCommandQueryFramework/ServiceCollectionExtensions.cs
+++ SimpleCommandQueryFramework/ServiceCollectionExtensions.cs
@@ -11,5 +11,23 @@ namespace SimpleCommandQueryFramework
 
             return serviceCollection;
         }
+
+        public static IServiceCollection RegisterCommandHandler<TCommand, THandler>(this IServiceCollection serviceCollection)
+            where TCommand : ICommand
+            where THandler : class, ICommandHandler<TCommand>
+        {
+            serviceCollection.AddTransient<ICommandHandler<TCommand>, THandler>();
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection RegisterQueryHandler<TQuery, TResult, THandler>(this IServiceCollection serviceCollection)
+            where TQuery : IQuery
+            where THandler : class, IQueryHandler<TQuery, TResult>
+        {
+            serviceCollection.AddTransient<IQueryHandler<TQuery, TResult>, THandler>();
+
+            return serviceCollection;
+        }
     }
 }
